e57b99fd3620ba05dad2e258334e958dfe7a9583
Anthony Holder <anthonyh202x@gmail.com>
2020-01-02 05:29:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
@@ -18,4 +19,5 @@ def nyc_pigeon_organizer(data)
     end
   end
   new_hash
+  binding.pry
 end
